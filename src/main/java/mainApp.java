507285e041cd5bc522f4614573df896ba3347abc
sAyflort <neefe.saratan.life.life@gmail.com>
2022-02-08 08:19:27 +0300

    add Homework 2-4 (new)

--- src/main/java/mainApp.java
+++ src/main/java/mainApp.java
@@ -1,5 +1,43 @@
 public class mainApp {
-    public static void main(String[] args) {
+    private static final Object mon = new Object();
+    private static char currentLetter = 'A';
 
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            createThread();
+        }
+    }
+
+    public static void createThread() {
+        new Thread(() -> {
+            synchronized (mon) {
+                final char c = currentLetter;
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != c) {
+                        try {
+                            mon.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println(currentLetter);
+                    switch (currentLetter) {
+                        case 'A': {
+                            currentLetter = 'B';
+                            break;
+                        }
+                        case 'B': {
+                            currentLetter = 'C';
+                            break;
+                        }
+                        case 'C': {
+                            currentLetter = 'A';
+                            break;
+                        }
+                    }
+                        mon.notifyAll();
+                }
+            }
+        }).start();
     }
 }
